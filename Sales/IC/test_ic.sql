905339091bc82cdcef0c1dfff2e7939c14abc53a
rineshr <rineshr@bajajauto.co.in>
2025-09-29 16:37:10 +0530

    deleted old files

--- Sales/IC/test_ic.sql
+++
@@ -1,6 +0,0 @@
-select  a.FollowupBucket , B.* 
-from ASM_TRIUMPH_ENQUIRY_DIM a
-join ASM_TRIUMPH_Enquiry_FACT b on a.PK_EnquiryHeaderID = b.FK_ENQUIRYDOCID
-where EnquiryDate between '2025-06-01' and '2025-06-30'
-and DEALERCODE in('0000014129')
-and b.LeadType in('Digital','Aggregators') and a.FollowupBucket=
--- Sales/PB/K+T/R3/UPDATED/DDL/K+T_No_of_Follow_ups_DDL.sql
+++
@@ -1,7 +0,0 @@
-
-alter table ASM_PB_T_ENQUIRY_DIM add No_of_Follow_ups int null
-
-alter Table ASM_PB_HK_ENQUIRY_DIM add No_of_Follow_ups int null
-
-alter Table ASM_PB_HKT_ENQUIRY_DIM add No_of_Follow_ups int null
-
